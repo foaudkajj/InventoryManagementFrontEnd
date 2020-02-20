a0043e5a3289c7704fec0a34890faad3ff5ffc9d
foaudkajj <fouadalikaj@gmail.com>
2020-02-20 18:05:14 +0300

    first commit

---
+++ src/app/navigation/navigation.ts
@@ -0,0 +1,48 @@
+import { FuseNavigation } from '@fuse/types';
+
+export const navigation: FuseNavigation[] = [
+    {
+        id: "mainpage",
+        title: 'Ana Sayfa',
+        icon: 'flaticon2-architecture-and-city',
+        type: 'item',
+    },
+    {
+        id: 'sellingModule',
+        title: 'Satış Modulü',
+        // translate: 'NAV.APPLICATIONS',
+        type: 'collapsable',
+        children: [
+            {
+                id: 'normal_sale',
+                title: 'Normal Satış',
+                // translate: 'NAV.SAMPLE.TITLE',
+                type: 'item',
+                // icon: 'email',
+                url: '/dashboard'
+            },
+            {
+                id: 'recipt_sale',
+                title: 'Faturalı Satış',
+                type: 'item',
+                // icon: 'flaticon-price-tag',
+                url: '/dashboard'
+                // translate: 'MENU.SELLING',
+            },            {
+                id: 'recipt_sale',
+                title: 'İade',
+                type: 'item',
+                // icon: 'flaticon-price-tag',
+                url: '/dashboard'
+                // translate: 'MENU.SELLING',
+            },            {
+                id: 'recipt_sale',
+                title: 'Değişim',
+                type: 'item',
+                // icon: 'flaticon-price-tag',
+                url: '/dashboard'
+                // translate: 'MENU.SELLING',
+            },
+        ]
+    }
+];
